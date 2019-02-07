db4e87c238bcdaee936c1799dd79d08a3bf4a92d
KingDuke0187 <KingDuke0187@users.noreply.github.com>
2019-02-07 14:48:21 -0400

    Added source file

---
+++ Test1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+int main()
+string s0
+string s1 = "hello";
+string s2 = s1 + "world";
+
+//printing
+cout << "s0 =" s0 <<endl;
+cout << "s1 =" <<s1 <<endl;
+cout << "the 4th character of s1 is" << s1{3}<<endl;
+cout << "s1 has" << s1.size() <<"characters" <<endl;
+
+//comparison
+if(s1 == "hello")
+cout << "hi there" <<endl;
+if(s2 ! == "world")
+cout<<"out of this world" <<endl;
+
+s1 += "Kitty"
