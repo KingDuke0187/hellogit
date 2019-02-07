59afb0cdfdc45d31943a3f05158d6d8a86c272d5
KingDuke0187 <KingDuke0187@users.noreply.github.com>
2019-02-07 14:52:08 -0400

    Added source file

--- Test1.cpp
+++ Test1.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
-#include <string>
+#include <random> //allow access to C++ random facilities
+
+using namespace std;
+
+
 int main()
-string s0
-string s1 = "hello";
-string s2 = s1 + "world";
+{
+    std::random_device rd; // will be used to seed the engine eng1 below
+    std::mt19937 eng1(rd()); // this engine produces different ouput every time
+    std::mt19937 eng2(1337);// this engine  produces the same output every time
 
-//printing
-cout << "s0 =" s0 <<endl;
-cout << "s1 =" <<s1 <<endl;
-cout << "the 4th character of s1 is" << s1{3}<<endl;
-cout << "s1 has" << s1.size() <<"characters" <<endl;
+    float mean = 36.7f; // average
+    float sd = 4.565f; // standard deviation
 
-//comparison
-if(s1 == "hello")
-cout << "hi there" <<endl;
-if(s2 ! == "world")
-cout<<"out of this world" <<endl;
+    int min = 10; // minimum number to generate
+    int max = 48; // maximum number to generate
 
-s1 += "Kitty"
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<float> norm(mean,sd);
+    std::uniform_int_distribution<> unifrm(min, max);
+
+  //generate 20 normally distributed floats  
+  for(int n=0 ;  n < 20 ;  n++) 
+   {
+        cout<<norm(eng1)<<" "; // different output each time
+    }
+
+  //generate 20 uniformly distributed integers 
+  for(int n=0 ;  n < 20 ; n++) 
+  {
+        cout<<unifrm(eng2)<<" ";// same output each time
+    }
+
+
